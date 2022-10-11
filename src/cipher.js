0f24d2c30bce66e371f314fa8e6941a80a9ea84d
JENNYFERGAMBOA <jennyfergamboaaponte@gmail.com>
2022-10-11 11:18:07 -0500

    commit_1

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,73 @@
-const cipher = {
-  // ...
-};
+const cipher={
+      //cifrado, los valores de encode son:offst y string
+      encode:function(offset,string){
+            //variable en la cual se ubica nuestro array de abc
+            let abecedario= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            //variable en la que estara el mensaje cifrado
+            let respuesta="";
+            //le sacamos el modulo a la llave en caso de ser negativo
+            offset=(offset%26+26%26);
+            
 
+      
+
+
+            if(string){
+                  //longitud del texto a cifrar
+                  for(let i=0;i<string.length;i++){
+                        //mirara si la letra si esta en el abc
+                        if(abecedario.indexOf(string[i]) !=-1){
+                              //variable guardara la posicon de nuestra letra
+                              //aqui se hace la suma de la letra con el offset y luego a llevo al modulo
+                              let posicion=(abecedario.indexOf(string[i])+offset)%26;
+                              //muestra el resultado concatenado
+                              respuesta+=abecedario[posicion];
+                        }else{
+                        respuesta+=string[i];
+                        }
+                  }
+            }
+            return respuesta;
+      },
+
+
+
+
+      
+     //los valores de decode son:offst y string
+       decode: function(offset,string){
+               //variable en la cual se ubica nuestro array de abc
+             let abecedario= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+             //variable en la que estara el mensaje cifrado
+             let respuesta="";
+             //le sacamos el modulo a la llave en caso de ser negativo
+             offset=(offset%26-26)%26;
+
+             if(string){
+                   //longitud del texto a cifrar
+                   for(let i=0;i<string.length;i++){
+                         //mirara si la letra si esta en el abc
+                         if(abecedario.indexOf(string[i]) !=-1){
+                               //variable guardara la posicon de nuestra letra
+                               //aqui se hace la suma de la letra con el offset y luego a llevo al modulo
+                               let posicion=(abecedario.indexOf(string[i])-offset)%26;
+                               //muestra el resultado concatenado
+                               respuesta+=abecedario[posicion];
+                         } else {
+                               respuesta+=string[i];
+                           }
+                   }
+             }
+             return respuesta;
+       }
+};
+//funcion para validar los numeros de desplazamiento ingresados por usuario
+function validarNumeros(event){
+      if(event.charCode>= 48 && event.charCode<= 57){
+            return true;
+      }
+            return false;
+}
+    validarNumeros;
+//console.log(cipher.value);   
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+window.addEventListener("load",inicio,true);
+//Función de inicio que captura los datos y transforma el  input string en mayusculas
+function inicio(){
+  document.getElementById("mensajeADecifrar").addEventListener("keyup",function(){
+    this.value=this.value.toUpperCase();
+  },
+  true
+  );
+
+  /*document.getElementById("desplazamiento").addEventListener("onkeypress",function(){
+    if(offset<0){
+    return alert("error, escoja un numero positivo");
+    }
+    return alert("puede avanzar")
+  });*/
+
+  document.getElementById("cifrar").addEventListener("click",function(){
+    let string= document.getElementById("mensajeADecifrar").value;
+    let offset= document.getElementById("desplazamiento").value;
+    document.getElementById("textoResultado").value=cipher.encode(offset,string);
+  },
+  true
+  );
+   
+  document.getElementById("descifrar").addEventListener("click",function(){
+    let string= document.getElementById("mensajeADecifrar").value;
+    let offset= document.getElementById("desplazamiento").value;
+    document.getElementById("textoResultado").value=cipher.decode(offset,string);
+  },
+  true
+  );
+}
+
+
+//console.log(cipher);
